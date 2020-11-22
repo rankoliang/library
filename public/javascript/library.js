72f40b684d5c3bf6aec6bd6877f38fb4715c8b7b
Ranko Liang <rankoliang@gmail.com>
2020-11-22 02:23:20 -0500

    Refactor card out of form class

--- public/javascript/library.js
+++ public/javascript/library.js
@@ -1,3 +1,11 @@
+const newBookButton = document.querySelector("#new-book");
+const books = document.querySelector("#books");
+const progress = document.querySelector("#progress");
+
+const library = new Library();
+
+// Book Class
+
 function Book(params) {
   this.title = params["title"];
   this.author = params["author"];
@@ -145,13 +153,15 @@ Book.prototype.cardHTML = function () {
   return book_card;
 };
 
+// Library Class
+
 function Library() {
   this.books = [];
 }
 
 Library.prototype.addBook = function (book) {
   this.books.push(book);
-  updateLibraryProgress();
+  this.updateProgress();
 };
 
 Library.prototype.booksRead = function () {
@@ -179,145 +189,193 @@ Library.prototype.progress = function () {
   return Math.round((this.pagesRead() * 100.0) / this.totalPages());
 };
 
-const newBookButton = document.querySelector("#new-book");
-const books = document.querySelector("#books");
-const progress = document.querySelector("#progress");
-const library = new Library();
-
-const updateLibraryProgress = () => {
-  progress.textContent = `${library.progress()}%`;
+Library.prototype.updateProgress = function () {
+  progress.textContent = `${this.progress()}%`;
 };
 
-const newForm = () => {
-  if (document.getElementById("new-book-form")) {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-    return;
+// Card class
+
+function Card(content) {
+  this.content = content;
+  this.element = document.createElement("div");
+  this.element.classList.add("relative", "pb-6", "card", "border-2");
+  if (typeof this.content.template === "function") {
+    this.element.innerHTML = this.content.template(this.template());
+  } else {
+    this.element.innerHTML = this.template();
   }
-  const form_card = document.createElement("div");
-  form_card.classList.add("relative", "pb-6", "border-gray-600", "card", "border-2");
-  form_card.innerHTML = `
-    <form>
+}
+
+Card.prototype.template = function () {
+  return `
+    ${this.content.image}
+    ${this.content.header}
+    ${this.content.subheader}
+    <div class="absolute left-0 flex flex-row-reverse items-center justify-between w-full px-2 text-sm top-2">
+      <span class="flex items-center">
+        ${this.content.info}
+      </span>
+      <div class="flex items-center h-auto">
+        ${this.content.buttons}
+      </div>
+    </div>
+  `;
+};
+
+// Form class
+
+function Form() {
+  this.image = `
+      <svg
+        class="h-48 m-auto"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+        />
+      </svg>
+  `;
+  this.header = `
+      <label for="title" class="hidden">Title</label>
+      <input
+        type="text"
+        placeholder="Title"
+        class="w-full text-xl font-semibold text-center text-gray-500 lg:text-base form-field"
+        id="title"
+        name="title"
+        required
+      />
+  `;
+  this.subheader = `
+      <label for="author" class="hidden">Author</label>
+      <input
+        type="text"
+        placeholder="author"
+        class="italic font-light text-center text-gray-400 truncate form-field lg:text-sm"
+        id="author"
+        name="author"
+        required
+      />
+  `;
+  this.info = `
+      <span class="flex items-center">
+        <label for="current-page" class="hidden">Current page</label>
+        <input type="number" min="0" class="w-12 text-center form-field" id="current-page" value="0" name="currentPage"/>
+        <label for="total-pages" class="hidden">Total pages</label>
+        /<input
+          type="number"
+          min="1"
+          class="w-12 mr-1 text-center form-field"
+          id="total-pages"
+          name="numPages"
+          value="1"
+        />
+      </span>
+  `;
+  this.buttons = `
+      <button
+        class="text-green-400 cursor-pointer hover:text-green-600 focus:text-green-600 focus:outline-none"
+        id="submit-book"
+        >
         <svg
-          class="h-48 m-auto"
-          xmlns="http://www.w3.org/2000/svg"
+          class="w-5 h-5"
           fill="none"
-          viewBox="0 0 24 24"
           stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
+        </svg>
+      </button>
+      <button class="text-red-400 focus:outline-none focus:text-red-600 hover:text-red-600" id="cancel-submission">
+        <svg
+          class="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
-          />
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
         </svg>
-        <label for="title" class="hidden">Title</label>
-        <input
-          type="text"
-          placeholder="Title"
-          class="w-full text-xl font-semibold text-center text-gray-500 lg:text-base form-field"
-          id="title"
-          name="title"
-          required
-        />
-        <label for="author" class="hidden">Author</label>
-        <input
-          type="text"
-          placeholder="author"
-          class="italic font-light text-center text-gray-400 truncate form-field lg:text-sm"
-          id="author"
-          name="author"
-          required
-        />
-        <div class="absolute left-0 flex flex-row-reverse items-center justify-between w-full px-2 text-sm top-2">
-          <span class="flex items-center">
-            <label for="current-page" class="hidden">Current page</label>
-            <input type="number" min="0" class="w-12 text-center form-field" id="current-page" value="0" name="currentPage"/>
-            <label for="total-pages" class="hidden">Total pages</label>
-            /<input
-              type="number"
-              min="1"
-              class="w-12 mr-1 text-center form-field"
-              id="total-pages"
-              name="numPages"
-              value="1"
-            />pages
-          </span>
-          <div class="flex items-center h-auto">
-            <button
-              class="text-green-400 cursor-pointer hover:text-green-600 focus:text-green-600 focus:outline-none"
-              id="submit-book"
-              >
-              <svg
-                class="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-              </svg>
-            </button>
-            <button class="text-red-400 focus:outline-none focus:text-red-600 hover:text-red-600" id="cancel-submission">
-              <svg
-                class="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </form>`;
-  const currentPageField = form_card.querySelector("#current-page");
-  const numPagesField = form_card.querySelector("#total-pages");
-  updateFieldWidth(currentPageField);
-  updateFieldWidth(numPagesField);
+      </button>`;
+}
+
+Form.prototype.template = function (template) {
+  return `
+    <form>
+      ${template}
+    </form>
+  `;
+};
+
+Form.prototype.configurePageFields = function (card) {
+  const currentPageField = card.querySelector("#current-page");
+  const numPagesField = card.querySelector("#total-pages");
+  this.updateFieldWidth(currentPageField);
+  this.updateFieldWidth(numPagesField);
 
   currentPageField.addEventListener("input", (e) => {
-    updateFieldWidth(e.target);
+    this.updateFieldWidth(e.target);
   });
 
   numPagesField.addEventListener("input", (e) => {
     currentPageField.max = e.target.value;
-    updateFieldWidth(e.target);
+    this.updateFieldWidth(e.target);
   });
+};
 
-  form_card.querySelector("#current-page").addEventListener;
-  form_card.querySelector("#cancel-submission").addEventListener("click", (e) => {
+Form.prototype.configureButtons = function (card) {
+  card.querySelector("#cancel-submission").addEventListener("click", (e) => {
     e.preventDefault();
-    form_card.remove();
+    card.remove();
   });
 
-  form_card.querySelector("form").addEventListener("submit", (e) => {
+  card.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     const bookParams = {};
-    for (const field of form_card.querySelectorAll("input")) {
+    for (const field of e.target.querySelectorAll("input")) {
       bookParams[field.name] = field.value;
     }
     const book = new Book(bookParams);
     library.addBook(book);
-    form_card.remove();
+    card.remove();
     books.appendChild(book.cardHTML());
   });
-  form_card.id = "new-book-form";
-  books.appendChild(form_card);
-  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
 };
 
-function updateFieldWidth(field) {
+Form.prototype.card = function () {
+  const card = new Card(this).element;
+  card.id = "new-book-form";
+  this.configurePageFields(card);
+  this.configureButtons(card);
+  return card;
+};
+
+Form.prototype.updateFieldWidth = function(field) {
   field.style.width = `${Math.max(field.value.length + 1, 2)}ch`;
 }
 
-newBookButton.addEventListener("click", newForm);
+newBookButton.addEventListener("click", () => {
+  if (document.getElementById("new-book-form")) {
+    // Scrolls to the bottom of the page
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    return;
+  }
+  books.appendChild(new Form().card());
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+});
+
 const book = new Book({
   title: "LOTR",
   author: "J.R.R. Tolkien",
@@ -327,7 +385,7 @@ const book = new Book({
 library.addBook(book);
 books.appendChild(book.cardHTML());
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 5; i++) {
   const book = new Book({
     title: "LOTR",
     author: "J.R.R. Tolkien",
